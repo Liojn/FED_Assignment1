170011c532ba6c1c4718045780dfb21fe9d81fb1
Liojn <s10259865@connect.np.edu.sg>
2023-11-26 21:01:30 +0800

    commit

---
+++ script.js
@@ -0,0 +1,39 @@
+/**************************Check if Sign In/ SIgn Up Filled before redirecting*************** */
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Function to validate and redirect
+  function signUp() {
+      var email = document.querySelector("input[type='email']").value;
+      var password = document.querySelector("input[type='password']").value;
+      var checkbox = document.querySelector("input[type='checkbox']");
+
+      // Check if email, password, and checkbox are filled or selected
+      if (email && password && checkbox.checked) {
+          // Redirect to index.html
+          window.location.href = "index.html";
+      } else {
+          // Display an alert or message for incomplete fields
+          alert("Please fill in all the required fields and agree to the terms of service.");
+      }
+  }
+
+  // Attach the signUp function to the button click event
+  var signUpButton = document.querySelector(".LogInBtn");
+  signUpButton.addEventListener("click", function (event) {
+      event.preventDefault(); // Prevent the form from submitting
+      signUp(); // Call the signUp function
+  });
+});
+
+
+/********************************** HamBurger Menu**************** */
+  document.addEventListener('DOMContentLoaded', function () {
+    const hamburgerBtn = document.getElementById('hamburger-btn');
+    const navList = document.querySelector('.nav-list');
+  
+    // Toggle the 'show-nav' class on the navList when the hamburger button is clicked
+    hamburgerBtn.addEventListener('click', function () {
+      navList.classList.toggle('show-nav');
+    });
+  });
+  
